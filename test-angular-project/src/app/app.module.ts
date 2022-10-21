6b468080b81ced925ddcfa178e25c6f2b854e938
ElManchacho <pld.pro@hotmail.com>
2022-10-21 08:54:58 +0200

    innstalled bulma + added components

--- test-angular-project/src/app/app.module.ts
+++ test-angular-project/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { PostListComponent } from './components/post-list/post-list.component';
+import { UserListComponent } from './components/user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainPageComponent,
+    PostListComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
