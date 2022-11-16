83f9efd4287b4bcc235cf9f65ecf973bdbb43e06
ElManchacho <pld.pro@hotmail.com>
2022-11-16 13:50:46 +0100

    service functions + user api calls ok & displayed

--- test-angular-project/src/app/components/user-list/user-list.component.ts
+++ test-angular-project/src/app/components/user-list/user-list.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { UserService } from 'src/app/@shared/services/user/user.service';
 
 @Component({
   selector: 'app-user-list',
@@ -9,34 +7,9 @@ import { UserService } from 'src/app/@shared/services/user/user.service';
 })
 export class UserListComponent implements OnInit {
 
-  userPseudosList:string[];
-
-  searchedUser:any;
-
-  postForm = new FormGroup({
-    pseudo: new FormControl("", Validators.required)
-  });
-
-  getUsersPseudoPromise:Promise<string[]> | undefined
-
-  constructor(private userService:UserService) {
-    this.userPseudosList = []
+  constructor() {
   }
 
-  ngOnInit(): void {
-    this.getUsersPseudoPromise = this.userService.getUsersPseudos()
-
-    this.getUsersPseudoPromise.then((value)=>{
-      this.userPseudosList = value
-    })
-  }
-
-  onSubmit(){
-    const searchPseudo:string = this.postForm.get("pseudo")?.value
-    const searchUser = this.userService.getUserByPseudo(searchPseudo)
-    searchUser.then((value)=>{
-      this.searchedUser = value
-    })
-  }
+  ngOnInit(): void { }
 
 }
--- test-angular-project/src/app/components/user/user.component.ts
+++ test-angular-project/src/app/components/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UserService } from 'src/app/@shared/services/user/user.service';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  userPseudosList:string[];
+
+  searchedUser:any;
+
+  postForm = new FormGroup({
+    pseudo: new FormControl("", Validators.required)
+  });
+
+  getUsersPseudoPromise:Promise<string[]> | undefined
+
+  constructor(private userService:UserService) {
+    this.userPseudosList = []
+  }
 
   ngOnInit(): void {
+    this.getUsersPseudoPromise = this.userService.getUsersPseudos()
+
+    this.getUsersPseudoPromise.then((value)=>{
+      this.userPseudosList = value
+    })
+  }
+
+  onSubmit(){
+    const searchPseudo:string = this.postForm.get("pseudo")?.value
+    const searchUser = this.userService.getUserByPseudo(searchPseudo)
+    searchUser.then((value)=>{
+      this.searchedUser = value
+    })
   }
 
 }
